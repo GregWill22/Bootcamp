d31cc8d371eed95b0d40c59625bce93873210dc7
GregWill22 <gregorywilliamms@gmail.com>
2022-08-29 12:27:20 -0400

    commit part1-react

---
+++ part1/src/App.js
@@ -0,0 +1,14 @@
+import Mensaje from './Mensaje'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Mensaje mensaje = "Hola que tal " color = "red"/>
+     
+    </div>
+  );
+}
+
+export default App;
