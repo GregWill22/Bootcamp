7273ba7bf218ceb2542e7a42f722af336648c2d2
GregWill22 <gregorywilliamms@gmail.com>
2022-08-29 17:28:16 -0400

    commit 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Header = ({ course }) => <h1>{course}</h1>;
+const Header = ({ course }) => {
+  console.log(course)
+ 
+  return ( 
+    <h1>{course}</h1>
+    );
+}
 
 const Part = (props)=>{
   return <p>{props.part} {props.exercises}</p>
@@ -23,18 +29,25 @@ const Total = ({exercises1,exercises2,exercises3}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-   const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1 = {part1} part2 = {part2}  part3={part3} exercises1 ={exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
-      <Total exercises1 = {exercises1} exercises2={exercises2} exercises3 = {exercises3} />
+      <Content part1 = {part1.name} part2={part2.name} part3 ={part3.name}
+       exercises1 ={part1.exercises} exercises2 ={part2.exercises} exercises3 ={part3.exercises1}/>
+       <Total exercises1={part1.exercises} exercises2 ={part2.exercises} exercises3 ={part3.exercises}/>
     </div>
   );
 };
